9454e8a888210ec904400819790311432c33ce79
nicoWeb31 <nico.riot@free.fr>
2021-02-16 16:07:43 +0100

    reducer interface action

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -5,9 +5,35 @@ interface Repositoriesstate {
 }
 
 
-const reducer = (state,action)=>{
-
+interface SearchRepoAction {
+    type: "SEARCH_REPO";
 }
 
+interface SearchRepoSuccessAction {
+    type: "SEARCH_REPO_SUCCESS";
+    payload: string[];
+}
+
+interface SearchRepoErrorAction {
+    type: "SEARCH_REPO_ERROR";
+    payload: string;
+}
+
+const reducer = (
+    state: Repositoriesstate,
+    action: SearchRepoAction | SearchRepoSuccessAction | SearchRepoErrorAction
+): Repositoriesstate => {
+    switch (action.type) {
+        case "SEARCH_REPO":
+            return { loading: true, error: null, data: [] };
+        case "SEARCH_REPO_SUCCESS":
+            return { loading: false, error: null, data: action.payload };
+        case "SEARCH_REPO_ERROR":
+            return { loading: false, error: action.payload, data: [] };
+
+        default:
+            return state;
+    }
+};
 
 export default reducer;
